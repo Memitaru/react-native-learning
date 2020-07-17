a57a60a1b707b7c944d7f1ad9ea01b99fab36fa3
Ami Scott <amiezrascott@gmail.com>
2020-07-16 22:58:58 -0700

    Added styles to to-do app

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.goalInput} />
+        <Button title="ADD" />
+      </View>
+      <View></View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center"
+  },
+  goalInput: {
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+    padding: 5,
+    width: "80%"
+  }
 });
