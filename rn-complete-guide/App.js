029b3e7fc3c92ec1104b80e787bb30df58c662c0
Ami Scott <amiezrascott@gmail.com>
2020-07-16 23:59:43 -0700

    Added list components

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,15 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from "react-native";
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, { uid: Math.random().toString(), value: enteredGoal }])
+    setEnteredGoal('')
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.goalInput} />
-        <Button title="ADD" />
+        <TextInput 
+          placeholder="Course Goal" 
+          style={styles.goalInput} 
+          onChangeText={goalInputHandler} 
+          value={enteredGoal} />
+        <Button 
+          title="ADD" 
+          onPress={addGoalHandler}
+        />
       </View>
-      <View></View>
+    <FlatList 
+      data={courseGoals} 
+      keyExtractor={(item, index) => item.uid}
+      renderItem={itemData => (
+        <View style={styles.listItem}>
+          <Text>{itemData.item.value}</Text>
+        </View>
+      )} 
+    />
 
       <StatusBar style="auto" />
     </View>
@@ -30,5 +58,12 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     padding: 5,
     width: "80%"
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginVertical: 10
   }
 });
